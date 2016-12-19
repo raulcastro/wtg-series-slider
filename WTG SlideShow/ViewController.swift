57f1fdcb9053061520ebc045904060db214088e9
Raul Castro <Raul Castro>
2016-12-19 00:25:10 -0500

    some thing

--- WTG SlideShow/ViewController.swift
+++ WTG SlideShow/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Aha it's a new change");
     }
 
     override func didReceiveMemoryWarning() {
